eed3b7708223a41d26adae988a171a4f12f175b8
LiuKun <13655914562@163.com>
2023-10-24 23:25:57 +0800

    回復接口待解決

--- xxx_vue/src/composable/usePost.ts
+++ xxx_vue/src/composable/usePost.ts
@@ -49,7 +49,7 @@ export default function usePost() {
             method: 'post',
             data: commentModel.value
         })
-        
+        await findOne(pid)
         return res;
     }
 
