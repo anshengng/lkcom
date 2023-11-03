cd1baf2f1954878d0f16d27045616f1efd13ddd9
LiuKun <13655914562@163.com>
2023-11-03 23:37:56 +0800

    Update center.ts

--- xxx_vue/src/router/module/center.ts
+++ xxx_vue/src/router/module/center.ts
@@ -1,17 +1,10 @@
 import { RouteRecordRaw } from 'vue-router';
 export default {
-    path: '/member',
-    name: 'member',
-    redirect: '/member/mail',
-    component: () => import('@/layouts/member.vue'),
-    meta: { menu: { title: '会员中心', icon: 'fab fa-etsy', active: true }, auth: true },
+    path: '/center',
+    name: 'center',
+    component: () => import('@/layouts/center.vue'),
+    meta: { menu: { title: '个人空间', icon: 'fab fa-etsy', active: true }, auth: true },
     children: [
-        {
-            path: 'mail',
-            name: 'member_center',
-            component: () => import('@/views/member/mail.vue'),
-            meta: { menu: { title: '资料管理' } },
-        },
         
     ]
 } as RouteRecordRaw
